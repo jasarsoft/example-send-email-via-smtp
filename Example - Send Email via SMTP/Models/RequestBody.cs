648f862b8fc59a8f6026a9b67c41e506a0db2bb7
Edin Jašarević <edinjasar14@gmail.com>
2022-09-29 20:00:29 +0200

    an example of how to send email

---
+++ Example - Send Email via SMTP/Models/RequestBody.cs
@@ -0,0 +1,20 @@
+﻿using Jasarsoft.Example.SendEmail.Models;
+using Newtonsoft.Json;
+
+namespace Jasarsoft.Example.SendEmail.Models;
+
+[Serializable]
+public sealed class RequestBody
+{
+    [JsonProperty("sender")]
+    public RequestEmailInfoModel Sender { get; set; }
+
+    [JsonProperty("to")]
+    public List<RequestEmailInfoModel> To { get; set; }
+
+    [JsonProperty("htmlContent")]
+    public string HtmlContent { get; set; }
+
+    [JsonProperty("subject")]
+    public string Subject { get; set; }
+}
---
+++ Example - Send Email via SMTP/Models/RequestEmailInfoModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Jasarsoft.Example.SendEmail.Models;
+
+[Serializable]
+public sealed class RequestEmailInfoModel
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    public RequestEmailInfoModel(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+}
