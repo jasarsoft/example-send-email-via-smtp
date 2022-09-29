7447cfba9eec762c6d06a6f4caadeb7431b3b8ca
Edin Jašarević <edinjasar14@gmail.com>
2022-09-29 19:59:24 +0200

    An example of how to send a email

--- Example - Send Email via SMTP/Program.cs
+++ Example - Send Email via SMTP/Program.cs
@@ -1,3 +1,48 @@
 ﻿
-Console.WriteLine("Example: Send email message via SMTP with MailKit");
+// Application code should start here.
 
+using Jasarsoft.Example.SendEmail;
+using Jasarsoft.Example.SendEmail.Models;
+using RestSharp;
+
+// Example: How to send email via SMTP protocol with MailKit.
+// We use SendinBlue, RestSharp and Newtonsoft;
+// Documentation: https://developers.sendinblue.com/reference/createemailcampaign-1
+Console.WriteLine("Example: Send email message via SMTP with MailKit\n");
+
+// Replace with your API Key value
+const string API_KEY = "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";
+
+// Create a rest client
+var client = new RestClient("https://api.sendinblue.com/v3/smtp/email");
+
+// Create a request
+var request = new RestRequest("", Method.Post);
+request.AddHeader("Accept", "application/json");
+request.AddHeader("content-type", "application/json");
+request.AddHeader("api-key", API_KEY);
+
+// Create a body
+var body = new RequestBody()
+{
+    Sender = new RequestEmailInfoModel("SenderName", "sendername@jasarsoft.com"),
+    To = new List<RequestEmailInfoModel>()
+    {
+        new ("ToName", "toname@jasarsoft.com")
+    },
+    Subject = "Subject email",
+    HtmlContent = "<!DOCTYPE html> <html> <body> <h1>Test email header</h1> <p>An email example html content</p> </body> </html>"
+};
+
+// Serialize the body and added to request;
+var restSerializer = new RestSerializer();
+request.AddJsonBody(restSerializer.Serialize(body));
+
+var response = client.Execute(request);
+
+// Write a response stats
+Console.WriteLine($"ID: {response.Content}");
+Console.WriteLine($"Successful: {response.IsSuccessful}");
+Console.WriteLine($"Response status: {response.ResponseStatus}");
+
+Console.ReadKey();
---
+++ Example - Send Email via SMTP/RestSerializer.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RestSharp.Serializers;
+
+namespace Jasarsoft.Example.SendEmail;
+
+public class RestSerializer : ISerializer
+{
+    private readonly JsonSerializer _serializer;
+
+    public RestSerializer()
+    {
+        _serializer = new JsonSerializer
+        {
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Include
+        };
+    }
+
+    public string ContentType
+    {
+        get => "application/json";
+        set { }
+    }
+
+    public string Serialize(object obj)
+    {
+        using var stringWriter = new StringWriter();
+        using var jsonTextWriter = new JsonTextWriter(stringWriter)
+        {
+            QuoteChar = '"'
+        };
+
+        _serializer.Serialize(jsonTextWriter, obj);
+
+        var result = stringWriter.ToString();
+        return result;
+    }
+}
